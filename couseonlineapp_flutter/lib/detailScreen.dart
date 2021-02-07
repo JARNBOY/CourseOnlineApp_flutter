9b75efb1d9c16bca633774373df91faa7a97ac6f
tattoo197@outlook.com <papon@wecomics.in.th>
2021-02-07 15:54:55 +0700

    add custom view Clipper

--- couseonlineapp_flutter/lib/detailScreen.dart
+++ couseonlineapp_flutter/lib/detailScreen.dart
@@ -1,3 +1,4 @@
+import 'package:couseonlineapp_flutter/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
@@ -15,6 +16,7 @@ class DetailScreen extends StatelessWidget {
         child: Padding(
           padding: EdgeInsets.only(left: 20, top: 50, right: 20),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -22,11 +24,44 @@ class DetailScreen extends StatelessWidget {
                   SvgPicture.asset("assets/icons/arrow-left.svg"),
                   SvgPicture.asset("assets/icons/more-vertical.svg")
                 ],
+              ),
+              SizedBox(height: 30),
+              ClipPath(
+                clipper: BestSellerClipper(),
+                child: Container(
+                  color: kBestSellerColor,
+                  padding:
+                      EdgeInsets.only(left: 10, top: 5, right: 20, bottom: 5),
+                  child: Text(
+                    "BestSeller".toUpperCase(),
+                    style: TextStyle(fontWeight: FontWeight.w600),
+                  ),
+                ),
               )
             ],
           ),
         ),
       ),
     );
+  }
+}
+
+class BestSellerClipper extends CustomClipper<Path> {
+  @override
+  getClip(Size size) {
+    // TODO: implement getClip
+    var path = Path();
+    path.lineTo(size.width - 20, 0);
+    path.lineTo(size.width, size.height / 2);
+    path.lineTo(size.width - 20, size.height);
+    path.lineTo(0, size.height);
+    path.lineTo(0, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper oldClipper) {
+    return false;
   }
 }
